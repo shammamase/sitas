ff3ec0fa43c4d4caa879dc6da6911d83e25db8d1
shammamase <smamase3@gmail.com>
2024-03-27 12:46:42 +0800

    upload logo dan loader

--- application/views/sitas/verif_surat/template_form.php
+++ application/views/sitas/verif_surat/template_form.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SITAS</title>
+  <title>LinTAS</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -41,7 +41,7 @@
 <div class="wrapper">
     
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="<?php echo base_url(); ?>/asset/lte31/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="<?php echo base_url(); ?>/asset/gambar/loader.png" alt="AdminLTELogo">
     </div>
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -73,8 +73,9 @@
             <!-- Brand Logo -->
         
             <a href="<?php echo site_url('primer/home') ?>" class="brand-link">
-              <img src="<?php echo base_url(); ?>asset/lte31/dist/img/AdminLTELogo.png" alt="Sijuara Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-              <span class="brand-text font-weight-light">SITAS</span>
+              <!--<img src="<?php echo base_url(); ?>asset/lte31/dist/img/AdminLTELogo.png" alt="Sijuara Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
+              <img src="<?php echo base_url(); ?>asset/gambar/lalu_lintas_data.png" alt="Sijuara Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+              <span class="brand-text font-weight-light">LinTAS</span>
             </a>
         
             <!-- Sidebar -->
